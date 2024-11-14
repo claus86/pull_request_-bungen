85b93a67125d884ac4bbac03f2f8cf38515b9e21
claudia sas <claudia.sas@jfks-portal.de>
2024-11-14 14:11:24 +0100

    Python-Datei mit if-Beispielen hinzugefügt

---
+++ if_beispiele.py
@@ -0,0 +1,10 @@
+# Dies ist eine Übungsdatei zu grundlegenden Befehlen in Python
+
+# Hier erkläre ich die if-Verzeigung mit einem Beispiel
+name1 = "alex"
+name2 = "kim"
+
+if name1 == "alex":
+    print('Die Variable enthält den String mit dem Namen "alex"')
+else: 
+    print('Die Variable enthält den String mit dem Namen "alex"')
